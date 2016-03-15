ed5ca51faa0fcc0541a51aa4c984940a5ed38ce6
Jhon Anderson Cardenas Diaz <jhonderson2007@gmail.com>
2016-03-15 18:50:52 -0500

    Implementing Update and Delete.

--- src/main/java/com/jhonacd/restfulexamples/messenger/resources/MessageResource.java
+++ src/main/java/com/jhonacd/restfulexamples/messenger/resources/MessageResource.java
@@ -3,8 +3,10 @@ package com.jhonacd.restfulexamples.messenger.resources;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -14,6 +16,8 @@ import com.jhonacd.restfulexamples.messenger.model.Message;
 import com.jhonacd.restfulexamples.messenger.service.MessageService;
 
 @Path("/messages")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class MessageResource {
 
 	private MessageService messageService = new MessageService();
@@ -25,15 +29,25 @@ public class MessageResource {
 	}
 
 	@POST
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_JSON)
 	public Message addMessage(Message pMessage) {
 		return messageService.addMessage(pMessage);
 	}
 
+	@PUT
+	@Path("/{messageId}")
+	public Message updateMessage(@PathParam("messageId") long pId, Message pMessage) {
+		pMessage.setId(pId);
+		return messageService.updateMessage(pMessage);
+	}
+
+	@DELETE
+	@Path("/{messageId}")
+	public Message deleteMessage(@PathParam("messageId") long pId) {
+		return messageService.removeMessage(pId);
+	}
+
 	@GET
 	@Path("/{messageId}")
-	@Produces(MediaType.APPLICATION_JSON)
 	public Message getMessage(@PathParam("messageId") long pId) {
 		return messageService.getMessage(pId);
 	}
